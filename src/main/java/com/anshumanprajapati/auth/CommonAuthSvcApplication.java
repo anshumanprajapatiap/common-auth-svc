9c5c0709b1c49b627ea3a9929dd6e1b36a2defa9
anshumanprajapati <anshumanprajapati2000@gmail.com>
2025-03-30 14:59:05 +0530

    Initial commit

---
+++ src/main/java/com/anshumanprajapati/auth/CommonAuthSvcApplication.java
@@ -0,0 +1,13 @@
+package com.anshumanprajapati.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonAuthSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonAuthSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshumanprajapati/auth/CommonAuthSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshumanprajapati.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonAuthSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
